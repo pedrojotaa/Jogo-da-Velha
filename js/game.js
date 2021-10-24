0d5b6b2ce75ae86153356d00bc28c2744b3a9027
Joao Pedro <eng.joao.pedro@outlook.com>
2021-10-24 02:41:07 -0700

    fixing errors

--- js/game.js
+++ js/game.js
@@ -1,13 +1,51 @@
-let board = ['', '', '', '', '', '', '', '']
+let board = ['', '', '', '', '', '', '', '', '']
 let playerTime = 0
 let symbol = ['o', 'x']
+let gameOver = false
+
+let winState = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+]
 
 function handleMove(position) {
-    board[position] = symbol[playerTime]
-
-    if(playerTime == 0){
-        playerTime = 1
-    }else{
-        playerTime = 0
+    if(gameOver){
+        return
     }
+
+    if(board[position] == ''){
+        board[position] = symbol[playerTime]
+        
+        gameOver = isWin()
+
+        if(gameOver == false){
+            if(playerTime == 0){
+                playerTime = 1
+            }else{
+                playerTime = 0
+            }
+        }
+    }
+    return gameOver 
+}
+
+function isWin(){
+    for(let i = 0; i < winState.length; i++){
+        let position = winState[i]
+
+        let pos1 = position[0]
+        let pos2 = position[1]
+        let pos3 = position[2]
+
+        if(board[pos1] == board[pos2] && board[pos1] == board[pos3] && board[pos1] != ''){
+            return true
+        }
+    }
+    return false
 }
--- js/interface.js
+++ js/interface.js
@@ -9,22 +9,14 @@ document.addEventListener('DOMContentLoaded', () => {
             let position = square.id
 
             handleMove(position)
-            updateSquare()
+            updateSquare(position)
         })
     })
-
-    function updateSquare(){
-        let squares = document.querySelectorAll('.square')
-
-        squares.forEach((square) => {
-            let position = square.id
-            let symbol = board[position]
-
-            if(symbol != ''){
-                square.innerHTML = `<div class= '${symbol}'></div> `
-            }
-        })
-    }
- 
 })
 
+function updateSquare(position){
+    let square = document.getElementById(position)
+    let symbol = board[position]
+
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
